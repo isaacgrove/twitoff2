8dbd8303562666617963582fb30c8fc73c9c5127
isaacgrove <isaacgrove333@gmail.com>
2020-09-14 19:12:38 -0400

    finished up monday

--- twitoff2/app.py
+++ twitoff2/app.py
@@ -10,7 +10,7 @@ def create_app():
   app = Flask(__name__)
 
   #for storing information in our database
-  app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite3'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
   #initilizes database within our app
